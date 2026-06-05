94e08e09a143d347e098fb805540970e79dc7d5d
aadarshjaiswal640 <aj5076@srmist.edu.in>
2026-06-05 11:00:42 +0530

    Task 1

---
+++ challenge1/app.py
@@ -0,0 +1,9 @@
+def connect_to_database():
+    print("Connecting to production database...")
+
+def call_aws_service():
+    print("Calling AWS S3 service...")
+
+if __name__ == "__main__":
+    connect_to_database()
+    call_aws_service()
